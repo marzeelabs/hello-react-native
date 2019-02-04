b465e7e2c18a5dcce411f7342f9b0d3cba6e4376
Luís Miguel <luism@marzeelabs.org>
2019-02-04 10:47:09 +0000

    Sign in screen layout

--- screens/SignIn.jsx
+++ screens/SignIn.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import {
   AsyncStorage,
-  Button,
   View,
 } from 'react-native';
+import {
+  Button,
+  Input,
+} from 'react-native-elements';
 import { connect } from 'react-redux';
 
 import { actionLogin } from '../actions/auth';
 
+import styles from './SignIn.scss';
+
 class SignIn extends Component {
   static navigationOptions = {
     title: 'Please sign in',
+    header: null,
   };
 
   _signInAsync = async () => {
@@ -27,8 +33,29 @@ class SignIn extends Component {
 
   render() {
     return (
-      <View>
-        <Button title="Sign in!" onPress={this._signInAsync} />
+      <View style={styles['sign-in__container']}>
+        <View>
+          <Input
+            autoComplete="username"
+            textContentType="username"
+            containerStyle={styles['sign-in__input-container']}
+            inputStyle={styles['sign-in__input']}
+            placeholder="Username"
+          />
+          <Input
+            autoComplete="password"
+            textContentType="password"
+            secureTextEntry
+            containerStyle={styles['sign-in__input-container']}
+            inputStyle={styles['sign-in__input']}
+            placeholder="Password"
+          />
+          <Button
+            buttonStyle={styles['sign-in__btn']}
+            title="Sign in!"
+            onPress={this._signInAsync}
+          />
+        </View>
       </View>
     );
   }
